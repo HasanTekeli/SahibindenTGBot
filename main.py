0a96cd16aea9953d358d447e3a4264b31a01b00a
Hasan A. Tekeli <hasan_tekeli@yahoo.com>
2021-06-10 17:13:45 +0300

    x-forwarded-for added

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ website = "https://www.sahibinden.com/satilik-arsa?pagingSize=50&a507_min=1500&s
 headers = {
     'Host': 'www.sahibinden.com',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36',
+    'X-Forwarded-For': '178.233.173.139',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'tr-TR,tr;q=0.9,en-TR;q=0.8,en;q=0.7,en-US;q=0.6',
